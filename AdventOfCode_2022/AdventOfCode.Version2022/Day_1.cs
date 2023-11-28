1bd84ccb4987a441f22ffbf6c11f61f23576b14d
StaPantazis <stathispantazis7@gmail.com>
2023-11-28 16:51:26 +0100

    Day 1

--- AdventOfCode_2022/AdventOfCode.Version2022/Day_1.cs
+++ AdventOfCode_2022/AdventOfCode.Version2022/Day_1.cs
@@ -4,34 +4,29 @@ using AdventOfCode.Core.Utils;
 
 namespace AdventOfCode.Version2022;
 
-public class Day_1 : AoCBaseDay<string, string, string[]>
+public class Day_1 : AoCBaseDay<string, string, int[]>
 {
     public override AoCSolution<string, string> Solve(AoCResourceType resourceType)
     {
-        var text = Helpers.File_ReadText(1, 2022, resourceType);
-
-        var cals = text.Split("\n\r\n").Select(x => x.Replace("\r", ""));
-        var calsTotals = cals.Select(x => x.Split('\n').Select(y => int.Parse(y)).Sum());
-        var calsTotals2 = calsTotals.OrderByDescending(x => x).ToArray();
-
-        Console.WriteLine(calsTotals2[0] + calsTotals2[1] + calsTotals2[2]);
-
-        var calss = Helpers.File_ReadText(1, 2022, resourceType)
+        var cals = Helpers.File_ReadText(1, 2022, resourceType)
             .Split("\n\r\n")
             .Select(x => x.Replace("\r", ""))
+            .Select(x => x.Split('\n')
+            .Select(y => int.Parse(y))
+            .Sum())
             .ToArray();
 
-        return new AoCSolution<string, string>(Part1(calss), Part2(calss));
+        return new AoCSolution<string, string>(Part1(cals), Part2(cals));
     }
 
-    protected override string Part1(string[] args)
+    protected override string Part1(int[] args)
     {
-        return args.Select(x => x.Split('\n').Select(y => int.Parse(y)).Sum()).First().ToString();
+        return args.OrderByDescending(x => x).First().ToString();
     }
 
-    protected override string Part2(string[] args)
+    protected override string Part2(int[] args)
     {
         var calcTotals = args.OrderByDescending(x => x).ToArray();
-        return calcTotals[0] + calcTotals[1] + calcTotals[2];
+        return (calcTotals[0] + calcTotals[1] + calcTotals[2]).ToString();
     }
 }
